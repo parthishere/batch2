6d892211de70f3cb24ad2e7238545bb65e61339f
“parthishere” <parthishere1234@gmail.com>
2023-08-07 14:43:32 +0530

    jfhakshdfkhas

---
+++ day5.py
@@ -0,0 +1,37 @@
+# functions
+
+# 25, 100000000
+
+# def add_mul(a, b):
+#     if (
+#         (isinstance(a, int) or isinstance(a, float)) 
+#         and
+#         (isinstance(b, int) or isinstance(b, float))
+#         ):
+        
+#         return a + b, a * b
+#     return None
+        
+# def mul(a, b):
+#     return a*b
+
+    
+# def main():
+#     a, b = add_mul(2 , 1.1)
+#     print(a, b)
+    
+
+# main()
+
+
+
+def add(a, b, c):
+    return a+ b+ c
+
+
+def another_main():
+    print(add(1, 2, 3, 4, 5, 6, 7, 8, 9))
+    
+
+if __name__ == "__main__":
+    another_main()
